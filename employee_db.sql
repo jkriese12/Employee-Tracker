723ebfef52992e3ee739a68db1810cd6f01b291c
Jacob Kriese <jkriese12@gmail.com>
2021-02-07 13:19:37 -0700

    Schema file creating database tables and boilerplate data added

---
+++ employee_db.sql
@@ -0,0 +1,80 @@
+-- Calling which database to use
+USE employee_db;
+
+-- Adding department titles as constants for the application
+INSERT INTO department (name)
+VALUES ("Sales");
+
+INSERT INTO department (name)
+VALUES ("Finance");
+
+INSERT INTO department (name)
+VALUES ("Legal");
+
+INSERT INTO department (name)
+VALUES ("HR");
+
+INSERT INTO department (name)
+VALUES ("Engineering");
+
+-- Adding all roles to database with salaries. ID's correspond with the ID of the department they belong to
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Rep", 40000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 50000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 55000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CFO", 150000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 75000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Paralegal", 37000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("HR Rep", 65000, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("HR Lead", 75000, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Developer", 80000, 5);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sr. Software Developer", 105000, 5);
+
+-- Adding Employees to Database for each role
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Smith", 1, 2);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Emily", "Hitchens", 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Luke", "Skywalker", 3, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Darth", "Vader", 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tony", "Stark", 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Peter", "Parker", 6, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Wanda", "Vision", 7, 8);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Dr.", "Strange", 8);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Thor", "GodOfThunder", 9, 10);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Tom", "Brady", 10);
---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_db;
+-- Calling creation of the Database
+CREATE DATABASE employee_db;
+-- Calling which Database to use for the next lines of code
+USE employee_db;
+-- Table creation for department, role and employee with unique information
+CREATE TABLE department (
+	id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
